855cda84de3e3891984026284c5bcefbab1a9950
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-20 17:07:52 +0500

    image_picker

--- lib/providers/image_pick.dart
+++ lib/providers/image_pick.dart
@@ -1,11 +1,20 @@
 import 'dart:io';
-
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
-class imagepicker extends ChangeNotifier {
+class ImageProviderClass extends ChangeNotifier {
+  final ImagePicker picked = ImagePicker();
   File? _image;
-Future  async (){
-image = await imagepicker(get.);
-} 
+  File? get image => _image;
 
+  Future<void> pckfrmglry() async {
+    final XFile? Pickedimg = await picked.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (Pickedimg != null) {
+      _image = File(Pickedimg.path);
+      notifyListeners();
+    }
+  }
 }
