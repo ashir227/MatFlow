d331578d1e3c6d2366a413e358250242d374e437
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-22 01:00:00 +0500

    BottomSheet

--- lib/Core/Widgets/bottomSheet.dart
+++ lib/Core/Widgets/bottomSheet.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:matflow/providers/image_pick.dart';
+import 'package:provider/provider.dart';
 
 void showImagesource(BuildContext context) {
   showModalBottomSheet(
     context: context,
     builder: (_) {
-      return Column();
+      return Column(
+        children: [
+          ListTile(
+            title: Icon(Icons.camera),
+            onTap: () {
+              context.read<ImagePckProvider>().pckfrmcamera();
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Icon(Icons.file_copy_outlined),
+            onTap: () {
+              context.read<ImagePckProvider>().pckfrmglry();
+            },
+          ),
+        ],
+      );
     },
   );
 }
