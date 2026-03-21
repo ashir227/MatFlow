9a440d6da7454ba07592ce0efbf11e117b7f5a6b
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-22 00:52:25 +0500

    BottomSheet

--- lib/Core/Widgets/bottomSheet.dart
+++ lib/Core/Widgets/bottomSheet.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:matflow/providers/image_pick.dart';
-import 'package:provider/provider.dart';
 
-class BottomSheets extends StatelessWidget {
-  const BottomSheets({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          onPressed: () {
-            Provider.of<ImagePckProvider>(
-              context,
-              listen: false,
-            ).pckfrmcamera();
-          },
-          icon: Icon(Icons.camera_alt),
-        ),
-        IconButton(
-          onPressed: () {
-            Provider.of<ImagePckProvider>(context, listen: false).pckfrmglry();
-          },
-          icon: Icon(Icons.file_copy_outlined),
-        ),
-      ],
-    );
-  }
+void showImagesource(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (_) {
+      return Column();
+    },
+  );
 }
