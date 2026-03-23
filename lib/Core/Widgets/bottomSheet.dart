d6b0dbb16d303738fb9c87a9fd23655d601af67a
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-23 22:35:48 +0500

    imagpck

--- lib/Core/Widgets/bottomSheet.dart
+++ lib/Core/Widgets/bottomSheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matflow/Core/theme/colors.dart';
 import 'package:matflow/providers/image_pick.dart';
 import 'package:provider/provider.dart';
 
@@ -9,14 +10,20 @@ void showImagesource(BuildContext context) {
       return Column(
         children: [
           ListTile(
-            title: Icon(Icons.camera),
+            // tileColor: Appcolor.Mat,
+            title: Icon(Icons.camera, color: Appcolor.Flow, size: 35),
             onTap: () {
               context.read<ImagePckProvider>().pckfrmcamera();
               Navigator.pop(context);
             },
           ),
           ListTile(
-            title: Icon(Icons.file_copy_outlined),
+            // tileColor: Appcolor.Mat,
+            title: Icon(
+              Icons.file_copy_outlined,
+              color: Appcolor.Flow,
+              size: 35,
+            ),
             onTap: () {
               context.read<ImagePckProvider>().pckfrmglry();
             },
