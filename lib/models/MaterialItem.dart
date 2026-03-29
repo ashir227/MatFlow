8ae925b39f4b42463f63656b61bd3ce12ed644e8
Ashir Ahmed <ashirahmed810@gmail.com>
2026-03-29 19:46:57 +0500

    Addmat logic

---
+++ lib/models/MaterialItem.dart
@@ -0,0 +1,12 @@
+class Materialitem {
+  String name;
+  int matinitstk;
+  int thresold;
+  int consumption;
+  Materialitem({
+    required this.name,
+    required this.matinitstk,
+    required this.thresold,
+    required this.consumption,
+  });
+}
