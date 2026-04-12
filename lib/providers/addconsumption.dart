7c4ae4ec2130d2798826c65769f41a14c184b953
Ashir Ahmed <ashirahmed810@gmail.com>
2026-04-12 13:34:21 +0500

    MatConsumption

---
+++ lib/providers/addconsumption.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:matflow/models/material_item.dart';
+
+class Consumeprovider extends ChangeNotifier {
+  String? PicesPro;
+  String? consumuption;
+  conumepro(Materialitem item, int stk) {}
+}
