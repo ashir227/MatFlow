39c70a5040397081d6c67dc3f7b92d4dac8162a3
Ashir Ahmed <ashirahmed810@gmail.com>
2026-04-12 13:29:13 +0500

    MatdConsumption

---
+++ lib/Screens/Consumemat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConMat extends StatelessWidget {
+  const ConMat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
